2c86f5800223cdf7b955e18f3ce1a84ba6ca8d83
Evan Woo <evawooevawoo@gmail.com>
2017-03-10 02:19:24 +0200

    validation_changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,11 @@ var minify = require('gulp-minifier');
 var pug = require('gulp-pug');
 var imagemin = require('gulp-imagemin');
 var autoprefixer = require('gulp-autoprefixer');
+var uglifycss = require('gulp-uglifycss');
 
 gulp.task('all:watch', function () {
-	gulp.watch('stylesheets/css/*.css', ['autoprefix']);
+	gulp.watch('css/style.css', ['autoprefix']);
+	gulp.watch('css/prefixed/*.css', ['compress:css']);
 	gulp.watch('js/script.js', ['compress:js']);
 	gulp.watch('views/**/*.pug', ['compile:pug']);
 	gulp.watch('images_big/**/*', ['img:min']);
@@ -25,6 +27,18 @@ gulp.task('compress:js', function () {
 		.pipe(gulp.dest('js/min'))
 });
 
+ 
+gulp.task('compress:css', function () {
+  gulp.src('css/prefixed/*.css')
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": false
+    }))
+    .pipe(gulp.dest('css/minifyed'));
+});
+
+
+
 gulp.task('compile:pug', function buildHTML() {
 	return gulp.src('views/templates/*.pug')
 		.pipe(pug({
@@ -33,6 +47,7 @@ gulp.task('compile:pug', function buildHTML() {
 
 });
 
+
 gulp.task('img:min', function () {
 	return gulp.src('images_big/**/*')
 		.pipe(imagemin())
@@ -40,9 +55,9 @@ gulp.task('img:min', function () {
 });
 
 gulp.task('autoprefix', function () {
-	gulp.src('css/*.css')
+	gulp.src('css/style.css')
 		.pipe(autoprefixer({}))
-		.pipe(gulp.dest('css/'))
+		.pipe(gulp.dest('css/prefixed/'))
 });
 
 
